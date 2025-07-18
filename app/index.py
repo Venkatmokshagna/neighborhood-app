1176fd44ca74bccc427754e7d60e2ef05597b186
Venkatmokshagna <mokshagna1522@gmail.com>
2025-07-18 09:41:45 +0530

    Initial project setup with Flask app

---
+++ app/index.py
@@ -0,0 +1,10 @@
+# app/index.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Neighborhood!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
